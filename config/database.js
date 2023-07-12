e5d3d34c9dfc4a6e87ab9894d4507457214f601f
mbcodes <mbcodes@protonmail.com>
2023-07-11 17:47:32 -0700

    removed db string

--- config/database.js
+++ config/database.js
@@ -3,9 +3,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(
-      "mongodb+srv://hooks:zZ6Q1D6TDXWhrVCJ@cluster0.xdav4qv.mongodb.net/?retryWrites=true&w=majority"
-    );
+    const conn = await mongoose.connect("");
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(error);
